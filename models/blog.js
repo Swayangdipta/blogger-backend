4774d1536f4950d9f453bb29a62aab4eb60f6053
Swayangdipta Das <swayangdiptacc@gmail.com>
2023-05-09 20:21:05 +0530

    Created blog model

---
+++ models/blog.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const { v1 } = require('uuid')
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        maxlength: 200
+    },
+    coverImage: {
+        type: String,
+        default: 'https://placekitten.com/640/320'
+    },
+    blog: [],
+    likes: {
+        type: Number,
+        default: 0
+    },
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }],
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+},{timestamps: true})
+
+module.exports = mongoose.model("Blog",blogSchema)
