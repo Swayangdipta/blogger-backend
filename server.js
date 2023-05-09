51899b093c927b4c8426db84ca40bd17ba9fc1ec
Swayangdipta Das <swayangdiptacc@gmail.com>
2023-05-09 18:58:45 +0530

    Configured server & database

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
 const { startServer } = require('./utils/server_start')
+const { dbConnection } = require('./utils/database_connnection')
+const app = express()
 
+// MiddleWares
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
 
+// Database Connection
+dbConnection()
 
-
+// Finally Starting the Server
 startServer()
---
+++ utils/database_connnection.js
@@ -0,0 +1,16 @@
+const config  = require('../config/config')
+const mongoose = require('mongoose')
+
+
+exports.dbConnection = () => {
+    try {
+        mongoose.connect(config.DB_URL).then(()=>{
+            console.log(`DATABASE CONNECTED.`);
+        }).catch(err => {
+            console.log(err);
+        })
+    } catch (error) {
+        console.error("Faild to establish database connection!!!");
+        console.error(error);
+    }
+}
