202ecfe841a19c5f1c7b700876d0334598cb5923
Swayangdipta Das <swayangdiptacc@gmail.com>
2023-05-09 21:40:35 +0530

    Added Auth Routes in server

--- server.js
+++ server.js
@@ -6,13 +6,19 @@ const { startServer } = require('./utils/server_start')
 const { dbConnection } = require('./utils/database_connnection')
 const app = express()
 
+// Route Imports
+const authRoutes = require('./routes/auth')
+
+// Database Connection
+dbConnection()
+
 // MiddleWares
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
 
-// Database Connection
-dbConnection()
+// Setting up routes
+app.use('/api',authRoutes)
 
 // Finally Starting the Server
 startServer()
