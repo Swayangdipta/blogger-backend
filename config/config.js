89a958c3f05a5ce4e4aca6afab11c16026411451
Swayangdipta Das <swayangdiptacc@gmail.com>
2023-05-10 22:09:40 +0530

    Added cloudinary variables

--- config/config.js
+++ config/config.js
@@ -1,7 +1,10 @@
 const config = {
     "PORT": process.env.PORT,
     "DB_URL": process.env.MONGODB_URL,
-    "SECRET": process.env.SECRET
+    "SECRET": process.env.SECRET,
+    "CLOUDINARY_CLOUD_NAME": process.env.CLOUDINARY_CLOUD_NAME,
+    "CLOUDINARY_API_KEY": process.env.CLOUDINARY_API_KEY,
+    "CLOUDINARY_API_SECCRET": process.env.CLOUDINARY_API_SECCRET
 }
 
 module.exports = config
