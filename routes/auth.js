cbdc1dc2d5d1c6836c794c778f9a8a40dba60614
Swayangdipta Das <swayangdiptacc@gmail.com>
2023-05-10 20:27:56 +0530

    Added routes for password forget and reset

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
-const { register, login } = require('../controllers/auth')
+const { register, login, getResetPasswordURL,resetPassword } = require('../controllers/auth')
 
 router.post('/auth/register',register)
 router.post('/auth/login',login)
+router.post('/auth/forget',getResetPasswordURL)
+router.post('/auth/reset',resetPassword)
 
 module.exports = router
